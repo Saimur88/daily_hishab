afbbeb51b00e449eb48243002f75da529e88e193
Saimur Rahman <saimur.cdk@gmail.com>
2026-02-21 02:42:46 +0600

    Added Swipe to delete income history

--- lib/widgets/income_history_list.dart
+++ lib/widgets/income_history_list.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import '../models/transaction.dart';
+import '../providers/transaction_provider.dart';
 
 class IncomeHistoryList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -19,25 +19,65 @@ class IncomeHistoryList extends StatelessWidget {
         textAlign: TextAlign.center,
         ),
       );
-  }
-    return ListView.builder(
+  } else {
+      return ListView.builder(
       itemCount: incomeTransactions.length,
         itemBuilder:(context, index){
-        final transaction = incomeTransactions[index];
-        return Card(
-          elevation: 2,
-          child: ListTile(
-            leading: Text('${index + 1}.', style: TextStyle(fontSize: 15)),
-            title: Text(
-              transaction.category,
-              style: TextStyle(fontWeight: FontWeight.w500),
+        final indexedTransaction = incomeTransactions[index];
+        return Dismissible(
+          key: ValueKey(indexedTransaction.id),
+          direction: DismissDirection.endToStart,
+          background: Container(color:  Colors.transparent,),
+          secondaryBackground: Container(
+            alignment: Alignment.centerRight,
+            color: Colors.red,
+            child: const Icon(Icons.delete_forever, color: Colors.white),
+          ),
+          confirmDismiss: (_) async {
+            return await showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: const Text('Delete Transaction'),
+                  content: const Text('This action cannot be undone.'),
+                  actions: [
+                    TextButton(
+                        onPressed: (){
+                          Navigator.of(context).pop(false);
+                        },
+                        child: const Text('Cancel')),
+                    TextButton(
+                        onPressed: (){
+                          Navigator.of(context).pop(true);
+                        },
+                        child: const Text('Delete',style: TextStyle(
+                          color: Colors.red,
+                        ),)),
+                  ],
+                ),
 
+            );
+          },
+          onDismissed: (_){
+            context.read<TransactionProvider>().deleteTransaction(
+              indexedTransaction.id,
+            );
+          },
+          child: Card(
+            elevation: 2,
+            child: ListTile(
+              leading: Text('${index + 1}.', style: TextStyle(fontSize: 15)),
+              title: Text(
+                indexedTransaction.category,
+                style: TextStyle(fontWeight: FontWeight.w500),
+
+              ),
+              trailing: Text(indexedTransaction.amount.toStringAsFixed(2),style: TextStyle(
+                fontSize: 15,
+              ),),
             ),
-            trailing: Text(transaction.amount.toStringAsFixed(2),style: TextStyle(
-              fontSize: 15,
-            ),),
           ),
         );
         } );
+    }
   }
 }
