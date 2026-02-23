b47a652bc28cd6971af089138e751caf7b3cf6a4
Saimur Rahman <saimur.cdk@gmail.com>
2026-02-24 00:33:13 +0600

    Invalid input error handled in textfield

--- lib/providers/add_transaction_provider.dart
+++ lib/providers/add_transaction_provider.dart
@@ -1,87 +1,92 @@
-import 'package:flutter/material.dart';
-import '../models/transaction.dart';
-import '../repositories/transaction_repository.dart';
+  import 'package:flutter/material.dart';
+  import '../models/transaction.dart';
+  import '../repositories/transaction_repository.dart';
 
-class AddTransactionProvider extends ChangeNotifier{
+  class AddTransactionProvider extends ChangeNotifier{
 
 
-  final TextEditingController controller = TextEditingController();
-  final TransactionRepository _repository = TransactionRepository();
+    final TextEditingController controller = TextEditingController();
+    final TransactionRepository _repository = TransactionRepository();
 
-  bool _isEditing = false;
-  bool get isEditing => _isEditing;
+    bool _isEditing = false;
+    bool get isEditing => _isEditing;
+    String? _errorMessage;
 
-  void setEditing(bool value) {
-    _isEditing = value;
+    String? get errorMessage => _errorMessage;
+
+    void setEditing(bool value) {
+      _isEditing = value;
+    }
+  Future<void> errorText() async {
+      _errorMessage = "Something went wrong";
+      notifyListeners();
   }
 
 
+    bool _initialized = false;
 
-  bool _initialized = false;
+    void loadFromTransaction(Transaction tx) async {
+      if(_initialized) return;
+      _isEditing = true;
+      controller.text = tx.amount.toString();
+      _type = tx.type;
+      _selectedCategory = tx.category;
+      _initialized = true;
+      notifyListeners();
+    }
 
-  void loadFromTransaction(Transaction tx) async {
-    if(_initialized) return;
-    _isEditing = true;
-    controller.text = tx.amount.toString();
-    _type = tx.type;
-    _selectedCategory = tx.category;
-    _initialized = true;
-    await _repository.updateTransaction(tx);
-    notifyListeners();
+    @override
+    void dispose(){
+      controller.dispose();
+      super.dispose();
+    }
+
+    //Bottom Sheet Materials
+
+    final List<String> _expenseCategories = ['Shopping', 'Entertainment', 'Other'];
+    final List<String> _incomeCategories = ['Salary', 'Bonus', 'Business','Other'];
+
+    TransactionType _type = TransactionType.expense;
+    String _selectedCategory = "Shopping";
+
+    void setType (TransactionType type ) async {
+      _type = type;
+      _selectedCategory = categories.first;
+      notifyListeners();
+    }
+
+    TransactionType get type => _type;
+    String get selectedCategory => _selectedCategory;
+
+    List<String> get categories =>
+        type == TransactionType.expense ? _expenseCategories : _incomeCategories;
+
+    String get header =>
+        type == TransactionType.expense ? "Add Expense" : "Add Income";
+
+    String get categoryLabel =>
+        type == TransactionType.expense ? "Expense Category" : "Income Category";
+
+
+    Future<void> addExpenseSheet() async {
+      // Firebase will come here later)
+      _type = TransactionType.expense;
+      _selectedCategory = _expenseCategories.first;
+      if(!_isEditing)controller.clear();
+      notifyListeners();
+    }
+
+    void changeSelected(String val){
+      _selectedCategory =  val;
+      notifyListeners();
+    }
+
+    Future<void> addIncomeSheet() async {
+      // Firebase will come here later)
+      _type = TransactionType.income;
+      _selectedCategory = _incomeCategories.first;
+      if(!_isEditing)controller.clear();
+      notifyListeners();
+    }
+
   }
-
-  @override
-  void dispose(){
-    controller.dispose();
-    super.dispose();
-  }
-
-  //Bottom Sheet Materials
-
-  final List<String> _expenseCategories = ['Shopping', 'Entertainment', 'Other'];
-  final List<String> _incomeCategories = ['Salary', 'Bonus', 'Business','Other'];
-
-  TransactionType _type = TransactionType.expense;
-  String _selectedCategory = "Shopping";
-
-  void setType (TransactionType type ) async {
-    _type = type;
-    _selectedCategory = categories.first;
-    notifyListeners();
-  }
-
-  TransactionType get type => _type;
-  String get selectedCategory => _selectedCategory;
-
-  List<String> get categories =>
-      type == TransactionType.expense ? _expenseCategories : _incomeCategories;
-
-  String get header =>
-      type == TransactionType.expense ? "Add Expense" : "Add Income";
-
-  String get categoryLabel =>
-      type == TransactionType.expense ? "Expense Category" : "Income Category";
-
-
-  Future<void> addExpenseSheet() async {
-    // Firebase will come here later)
-    _type = TransactionType.expense;
-    _selectedCategory = _expenseCategories.first;
-    if(!_isEditing)controller.clear();
-    notifyListeners();
-  }
-
-  void changeSelected(String val){
-    _selectedCategory =  val;
-    notifyListeners();
-  }
-
-  Future<void> addIncomeSheet() async {
-    // Firebase will come here later)
-    _type = TransactionType.income;
-    _selectedCategory = _incomeCategories.first;
-    if(!_isEditing)controller.clear();
-    notifyListeners();
-  }
-
-}
--- lib/widgets/add_transaction_sheet.dart
+++ lib/widgets/add_transaction_sheet.dart
@@ -1,131 +1,155 @@
-import 'package:daily_hishab/models/transaction.dart';
-import 'package:daily_hishab/providers/add_transaction_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+ import 'package:daily_hishab/models/transaction.dart';
+  import 'package:daily_hishab/providers/add_transaction_provider.dart';
+  import 'package:flutter/material.dart';
+  import 'package:provider/provider.dart';
 
-class AddTransactionSheet extends StatelessWidget {
-  final Transaction? existingTransaction; //gets the existing transaction when called to edit the transaction
-   const AddTransactionSheet({super.key, this.existingTransaction});
+  class AddTransactionSheet extends StatefulWidget {
+    final Transaction? existingTransaction; //gets the existing transaction when called to edit the transaction
+     const AddTransactionSheet({super.key, this.existingTransaction});
 
   @override
-  Widget build(BuildContext context) {
+  State<AddTransactionSheet> createState() => _AddTransactionSheetState();
+}
 
-    final provider = context.read<AddTransactionProvider>();
-    final header = context.select((AddTransactionProvider p) => p.header);
-    final selectedCategory = context.select((AddTransactionProvider p) => p.selectedCategory);
-    final type = context.select((AddTransactionProvider p) => p.type);
-    final categoryLabel = context.select((AddTransactionProvider p) => p.categoryLabel);
+class _AddTransactionSheetState extends State<AddTransactionSheet> {
 
-    final controller = context.read<AddTransactionProvider>().controller;
+    final _formKey = GlobalKey<FormState>();
+
+    @override
+    Widget build(BuildContext context) {
+
+      final provider = context.read<AddTransactionProvider>();
+      final header = context.select((AddTransactionProvider p) => p.header);
+      final selectedCategory = context.select((AddTransactionProvider p) => p.selectedCategory);
+      final type = context.select((AddTransactionProvider p) => p.type);
+      final categoryLabel = context.select((AddTransactionProvider p) => p.categoryLabel);
+
+      final controller = context.read<AddTransactionProvider>().controller;
 
 
-    return Padding(
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).viewInsets.bottom,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                TextButton(
-                    onPressed: (){
-                      context.read<AddTransactionProvider>().addExpenseSheet();
-                    },
-                    child: Text('Add Expense')
-                ),
-                Text("|",style: TextStyle(
-                  fontSize: 30,
-                ),),
-                TextButton(
-                    onPressed: () {
-                      context.read<AddTransactionProvider>().addIncomeSheet();
-                    },
-                    child: Text('Add Income')
-                )
-              ],
-            ),
-            Divider(
-              indent: 20,
-              endIndent: 20,
-              color: Colors.black,
-            ),
-            Center(
-              child: Text( header ,style: TextStyle(
-                fontSize: 20,
-              ),),
-            ),
-            SizedBox(height: 20,),
-            Row(
-              children: [
-                SizedBox(width: 30,height: 30,),
-                Text("Amount",style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15
-                ),),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20,right: 20),
-              child: TextField(
-                controller: controller,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  floatingLabelStyle: TextStyle(
-                    fontSize: 25,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20,),
-            Row(
-              children: [
-                SizedBox(width: 30,height: 30,),
-                Text(categoryLabel,style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15
-                ),),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20,right: 20),
-              child: DropdownButtonFormField(
-                initialValue: selectedCategory,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                ),
-                items: provider.categories.map((e) => DropdownMenuItem(value: e,child: Text(e),)).toList(),
-                borderRadius: BorderRadius.circular(20),
-                onChanged: (newValue){
-                    provider.changeSelected(newValue!);
-                },),
-            ),
-            SizedBox(height: 20,),
-            Center(child: ElevatedButton(
-                onPressed: () async {
-                  final transaction = Transaction(
-                      id: existingTransaction?.id ?? DateTime.now().toIso8601String(),
-                      amount: double.parse(controller.text),
-                      category: selectedCategory,
-                      type: type,
-                    timestamp: DateTime.now(),
-                  )
-                  ;
-                  Navigator.pop(context, transaction);
-                  }, child: Text(header))),
-
-          ],
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
         ),
-      ),
-    );
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                        onPressed: (){
+                          context.read<AddTransactionProvider>().addExpenseSheet();
+                        },
+                        child: Text('Add Expense')
+                    ),
+                    Text("|",style: TextStyle(
+                      fontSize: 30,
+                    ),),
+                    TextButton(
+                        onPressed: () {
+                          context.read<AddTransactionProvider>().addIncomeSheet();
+                          FocusScope.of(context).unfocus();
+                        },
+                        child: Text('Add Income')
+                    )
+                  ],
+                ),
+                Divider(
+                  indent: 20,
+                  endIndent: 20,
+                  color: Colors.black,
+                ),
+                Center(
+                  child: Text( header ,style: TextStyle(
+                    fontSize: 20,
+                  ),),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    SizedBox(width: 30,height: 30,),
+                    Text("Amount",style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15
+                    ),),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  child: TextFormField(
+                    validator: (value){
+                      final raw = value?.trim() ?? '';
+                      if(raw.isEmpty) return 'Please enter an amount';
+                      final amount = double.tryParse(raw);
+                      if (amount == null || amount <= 0) {
+                        return 'Please enter a valid amount';
+                      }
+                      return null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    controller: controller,
+                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                    decoration: InputDecoration(
+                      floatingLabelStyle: TextStyle(
+                        fontSize: 25,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                Row(
+                  children: [
+                    SizedBox(width: 30,height: 30,),
+                    Text(categoryLabel,style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15
+                    ),),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  child: DropdownButtonFormField(
+                    initialValue: selectedCategory,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )
+                    ),
+                    items: provider.categories.map((e) => DropdownMenuItem(value: e,child: Text(e),)).toList(),
+                    borderRadius: BorderRadius.circular(20),
+                    onChanged: (newValue){
+                        provider.changeSelected(newValue!);
+                    },),
+                ),
+                SizedBox(height: 16,),
+                Center(child: ElevatedButton(
+                    onPressed: (){
+                      final ok = _formKey.currentState?.validate() ?? false;
+                      if (!ok) return;
+                      final transaction = Transaction(
+                          id: widget.existingTransaction?.id ?? '',
+                          amount: double.parse(controller.text.trim()),
+                          category: selectedCategory,
+                          type: type,
+                        timestamp: DateTime.now(),
+                      );
+                      Navigator.pop(context, transaction);
 
-  }
+                      }, child: Text(header))),
+
+              ],
+            ),
+          ),
+        ),
+      );
+
+    }
 }
