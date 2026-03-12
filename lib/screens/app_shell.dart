44343551bf9876ff2e41638faf9c69f9254a7a1d
Saimur Rahman <saimur.cdk@gmail.com>
2026-03-13 03:33:52 +0600

    minor upgrade on bottomnavbar

--- lib/screens/app_shell.dart
+++ lib/screens/app_shell.dart
@@ -10,11 +10,21 @@ class AppShell extends StatelessWidget {
 
   final StatefulNavigationShell navigationShell;
 
+
   @override
   Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
     return Scaffold(
       body: navigationShell,
       bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.blueGrey,
+        indicatorColor: scheme.primaryContainer,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        labelTextStyle: WidgetStateProperty.all(
+          TextStyle(
+            fontWeight: FontWeight.bold,
+            color: scheme.onInverseSurface,),
+        ),
         selectedIndex: navigationShell.currentIndex,
         onDestinationSelected: (index) {
           navigationShell.goBranch(
@@ -24,13 +34,13 @@ class AppShell extends StatelessWidget {
         },
         destinations: [
            NavigationDestination(
-              icon: Icon(Ionicons.home_outline),
+              icon: Icon(Ionicons.home_outline,size: 25,),
                label: 'Home'),
           NavigationDestination(
-              icon: Icon(Ionicons.pie_chart_outline),
+              icon: Icon(Ionicons.pie_chart_outline,size: 25,),
               label: 'Statistics'),
           NavigationDestination(
-              icon: Icon(Ionicons.settings_outline),
+              icon: Icon(Ionicons.settings_outline,size: 25,),
               label: 'Settings'),
         ],
 
